165bd9afd0f2956b61522993a69819abe9a6eb24
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-29 22:14:37 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class uploaderPalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = uploaderPalculateWorker()
+    print("Worker engine initialized.")
